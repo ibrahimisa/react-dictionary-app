b82ca3f75e4fc825ff70f81051b02fb997deebcb
Ibrahim Isa Jajere <ibrahimisajajere@gmail.com>
2021-12-12 22:34:21 +0100

    fixed small issue

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const Bookmarks = (props) => {
+  console.log(props)
   return (
     <>
       {props.bookmarks.length === 0 ? (
         <h1>No Bookmarks</h1>
       ) : (
-        props.bookmarks.map((word) => {
+        props.bookmarks.map((word) => (
           <h2>{word.word}</h2>
-        })
+        ))
       )}
     </>
   );
